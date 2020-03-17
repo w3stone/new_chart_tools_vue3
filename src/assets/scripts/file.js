99cd26c02c271161d817282e69633f97cc0706cd
wjl <wjl@123.com>
2020-03-17 11:35:44 +0800

    补充图表类型选项

--- src/assets/scripts/file.js
+++ src/assets/scripts/file.js
@@ -26,17 +26,26 @@ var chartConfig = { //图表配置
 
 var charttypeOptions = [ //图表类型字典表
     {name:'柱状图', options:[
-        {name:'普通柱状图', type: 101, disabled:false},
-        {name:'柱状图+增长率', type: 102, disabled:false},
-        {name:'柱状图+折线图', type: 103, disabled:true},
-        {name:'普通柱状图（含平均值线）', type: 104, disabled:false},
-        {name:'柱状图百分比（相同x和为100%, 堆叠）', type: 105, disabled:false},
-        {name:'柱状图百分比（相同x和为100%）', type: 106, disabled:false},
-        {name:'柱状图百分比（相同name,即相同颜色和为100%）', type: 107, disabled:false},
-        {name:'柱状图百分比（相同name,即相同颜色和为100%）+增长率', type: 108, disabled:true},
-        {name:'柱状图+增长率（相同xdata之间的增长率）', type: 109, disabled:true},
-        {name:'普通柱状图堆叠', type: 110, disabled:false},
-        {name:'柱状图动态求和', type: 113, disabled:false}
+        {name:'普通柱状图101', type: 101, disabled:false},
+        {name:'柱状图+增长率102', type: 102, disabled:false},
+        {name:'柱状图+折线图103', type: 103, disabled:true},
+        {name:'普通柱状图（含平均值线）104', type: 104, disabled:false},
+        {name:'柱状图百分比（相同x和为100%, 堆叠）105', type: 105, disabled:false},
+        {name:'柱状图百分比（相同x和为100%）106', type: 106, disabled:false},
+        {name:'柱状图百分比（相同name,即相同颜色和为100%）107', type: 107, disabled:false},
+        {name:'柱状图百分比（相同name,即相同颜色和为100%）+增长率108', type: 108, disabled:true},
+        {name:'柱状图+增长率（相同xdata之间的增长率）109', type: 109, disabled:true},
+        {name:'普通柱状图堆叠110', type: 110, disabled:false},
+        {name:'柱状图动态求和113', type: 113, disabled:false}
+    ]},
+    {name:'横向柱状图', options:[
+        {name:'普通柱状图151', type: 151, disabled:false},
+        {name:'普通柱状图（含平均值线）154', type: 154, disabled:false},
+        {name:'柱状图百分比（相同x和为100%, 堆叠）155', type: 155, disabled:false},
+        {name:'柱状图百分比（相同x和为100%）156', type: 156, disabled:true},
+        {name:'柱状图百分比（相同name,即相同颜色和为100%）157', type: 157, disabled:true},
+        {name:'普通柱状图堆叠160', type: 160, disabled:false},
+        {name:'柱状图动态求和163', type: 163, disabled:false}
     ]},
     {name:'折线图', options:[
         {name:'普通折线图', type: 301, disabled:false},
